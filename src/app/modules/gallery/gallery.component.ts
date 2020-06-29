56f5a8e80c5286d733a500d65c037e55f2613a24
Just Imagine <johnfroi46@gmail.com>
2020-06-29 21:47:03 +0800

    [ADD][MODULES] gallery

---
+++ src/app/modules/gallery/gallery.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { IFile } from 'src/app/models/interfaces';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.scss']
+})
+export class GalleryComponent implements OnInit {
+  dataColumns: Array<Array<Array<IFile>>> = [];
+
+  @Input() set data(datas: any[]) {
+    const columns = [[], [], [], []];
+    let roundsCount = 0;
+
+    // Put the files in each column
+    datas.forEach(file_ => {
+      columns[roundsCount].push(file_);
+      roundsCount++;
+
+      if (roundsCount > 3) {
+        roundsCount = 0;
+      }
+    });
+
+    this.dataColumns = columns;
+  }
+
+  @Output()
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  delete() {
+    alert('delete');
+  }
+
+  edit() {
+    alert('edit');
+  }
+}
